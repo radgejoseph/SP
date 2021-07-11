962939c0427fc1078000bfafba836e5ae4ff6374
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-07-12 03:03:12 +0800

    test

--- connect.php
+++ connect.php
@@ -14,8 +14,9 @@
 //     "fKjE0CChQb",
 //     "3306");
 $conn = mysqli_connect(
-    "files.000webhost.com",
+    "localhost",
     "id15170702_speakupusername",
     "-Zbs7E<5N~}{f-Jz",
-    "id15170702_speakup");
+    "id15170702_speakup",
+    "3306");
 ?>
